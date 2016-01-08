70066ea68caf97e8608f2ce9f448f9302d7b0e96
clayton@xdevel.com.br <clayton@xdevel.com.br>
2016-01-08 15:17:04 -0200

    ajuste menores

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@
 let express = require('express');
 let router = express.Router();
 let passport = require('passport');
-const parcelas = require('../model/xdukaBoleto');
 const controller = require('../controller/invoice')();
+
 
 /* processa todos os boletos que nao criaram vinculo com iugu  */
 router.get('/boletos/gerar/:unidade/:year/:month',passport.authenticate('bearer', { session: false }), (req,res) => controller.processAllInvoices(req,res,false) );
@@ -12,10 +12,12 @@ router.get('/boletos/regerartodos/:unidade/:year/:month',passport.authenticate('
 /* regera boleto  */
 router.get('/boletos/regerar/:unidade/:movimento/:parcela',passport.authenticate('bearer', { session: false }), controller.processInvoice);
 /* processa retorno  */
-router.post('/boletos/retorno/:unidade/:movimento/:parcela', controller.invoiceStatusChange);
+router.post('/boletos/retorno/:unidade/:movimento/:parcela' , controller.invoiceStatusChange);
 /* tela para regerar boleto*/
 router.get('/boletos/atrasado/:unidade/:movimento/:parcela', controller.invoiceDueDate);
 router.get('/boletos/get/:unidade/:invoiceid',passport.authenticate('bearer', { session: false }), controller.showInvoice);
 
 
 module.exports = router;
+
+
